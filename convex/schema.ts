899cfca864da4654857488095e941507a70d70b1
Aadish Verma <aadish@ohs.stanford.edu>
2025-12-20 18:03:33 -0800

    feat: gut convex

--- convex/schema.ts
+++
@@ -1,9 +0,0 @@
-import { defineSchema, defineTable } from "convex/server";
-import { v } from "convex/values";
-
-export default defineSchema({
-  comments: defineTable({
-    slug: v.string(),
-    body: v.string(),
-  }).index("by_slug", ["slug"]),
-});
